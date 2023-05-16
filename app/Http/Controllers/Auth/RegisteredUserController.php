fd88a015fc86f444dbcb589a7b77fb6eba138865
FateevMaxim <fateev.maxim@gmail.com>
2023-05-16 15:24:01 +0600

    updates

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,7 +48,7 @@ class RegisteredUserController extends Controller
 
         $input = $request->code;
 
-        $pattern = "git init";
+        $pattern = "/\d{0,4}\(\d{0,4}\)/";
 
         $isSubstringPresent = preg_match($pattern, $input);
 
